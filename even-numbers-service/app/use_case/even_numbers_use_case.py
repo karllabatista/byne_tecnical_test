00b3b0508821d0beecfc7d4d42b469aee2694986
karllabatista <kblb@icomp.ufam.edu.br>
2025-07-28 15:19:09 -0400

    refactor use case

--- even-numbers-service/app/use_case/even_numbers_use_case.py
+++ even-numbers-service/app/use_case/even_numbers_use_case.py
@@ -1,11 +1,13 @@
+import random
+from app.infrastructure.even_numbers_infra import EvenNumbersInfra
 class EvenNumberUseCase:
 
-    def __init__(self,number:int):
-        self.number = number
+    def __init__(self):
+        self.number =random.randrange(10) 
 
 
-    def execute(self) -> bool:
-        
-        return self.calculate(self.number)
+    def execute(self,infra:EvenNumbersInfra) -> int:
+
+        return infra.calculate(self.number)
 
         
