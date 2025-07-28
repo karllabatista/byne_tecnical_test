a4f50d5587018328bed55ed0d4ce21b3755d761b
karllabatista <kblb@icomp.ufam.edu.br>
2025-07-28 16:12:44 -0400

    add even infra api

--- even-numbers-service/app/infrastructure/even_numbers_infra.py
+++ even-numbers-service/app/infrastructure/even_numbers_infra.py
@@ -1,4 +1,11 @@
+from app.domain.exceptions.is_not_even_exception import IsNotEvenException
 class EvenNumbersInfra:
 
     def calculate(self,number:int):
-        pass
+
+        try:
+            if number % 2 == 0:
+                return True
+        
+        except Exception as error:
+            raise IsNotEvenException("The number is not even") from error
