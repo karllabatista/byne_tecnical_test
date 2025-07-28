8dad257a7bb4081f6c089853467e87f9b5a7e38c
karllabatista <kblb@icomp.ufam.edu.br>
2025-07-28 14:31:30 -0400

    add router to return data

--- app/main.py
+++ app/main.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/test")
+@app.get("/helloworld")
 def test():
-    return {"message":"olaa mundo"}
+    return {"message":"hello world"}
+
+
+@app.get("/integer_info")
+def send_integer():
+    
+    return {"message":1}
